6e009fa7ade87329f5185a52cbd0822215f48d69
Sam <splavigne@gmail.com>
2018-07-05 21:40:57 -0400

    fix typo

--- Roles/client/main.py
+++ Roles/client/main.py
@@ -87,7 +87,7 @@ class Speech(threading.Thread):
 
                 all_audio = [BASE_PATH + '/audio/generation.wav']
                 all_audio += generation_files
-                all_audio = [BASE_PATH + '/audio/iteration.wav']
+                all_audio += [BASE_PATH + '/audio/iteration.wav']
                 all_audio += iteration_files
 
                 for audio_file in all_audio:
